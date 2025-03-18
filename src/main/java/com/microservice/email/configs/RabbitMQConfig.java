78223350a28bdcead6f67f38a1289de6a91796f7
bispobr <156684382+bispobr@users.noreply.github.com>
2025-03-18 10:30:02 -0300

    feat: adiciona config de conexão , email e conversão

---
+++ src/main/java/com/microservice/email/configs/RabbitMQConfig.java
@@ -0,0 +1,28 @@
+package com.microservice.email.configs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${broker.queue.email.name}")
+    private  String fila;
+
+    @Bean
+    public Queue queue(){
+        return new Queue(fila,true);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter(){
+        ObjectMapper objectMapper = new ObjectMapper();
+        return  new Jackson2JsonMessageConverter(objectMapper);
+    }
+}
