9a12cd8c6bfd183e7b516a639d4c8f35c5703036
bispobr <156684382+bispobr@users.noreply.github.com>
2025-07-13 16:43:17 -0300

    feat: Add logging ao  EmailService
    Introdução do registro SLF4J no EmailService para registrar eventos importantes durante o processamento de e-mails, incluindo operações de início, sucesso, erro e salvamento no banco de dados.

--- src/main/java/com/microservice/email/services/EmailService.java
+++ src/main/java/com/microservice/email/services/EmailService.java
@@ -4,6 +4,7 @@ import com.microservice.email.enums.StatusEmail;
 import com.microservice.email.models.EmailModel;
 import com.microservice.email.repositories.EmailRepository;
 import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
+@Slf4j
 @Service
 public class EmailService {
 
@@ -28,6 +30,7 @@ public class EmailService {
     @Transactional
     public EmailModel enviarEmail(EmailModel emailModel) {
         try{
+            log.info("tratando email");
             emailModel.setDataEnvioEmail(LocalDateTime.now());
             emailModel.setEmailDestinatario(emailDestinatario);
 
@@ -38,10 +41,14 @@ public class EmailService {
             javaMailSender.send(mensagem);
 
             emailModel.setStatusEmail(StatusEmail.enviado);
+            log.info("Email enviada");
         } catch (MailException e){
             emailModel.setStatusEmail(StatusEmail.erro);
+            log.error("Erro ao enviar email");
         } finally {
+            log.info("Email salvo na base de dados");
             return emailRepository.save(emailModel);
+
         }
     }
 }
