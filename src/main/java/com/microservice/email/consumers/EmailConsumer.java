0fdb6c864fb03c9923b575c85e45d363fabbb412
bispobr <156684382+bispobr@users.noreply.github.com>
2025-07-13 16:44:24 -0300

    feat: Add logging ao EmailConsumer
    adiciona  o registro SLF4J no Email Consumer e adicionamos uma instrução de registro de informações quando um e-mail é recebido da fila.

--- src/main/java/com/microservice/email/consumers/EmailConsumer.java
+++ src/main/java/com/microservice/email/consumers/EmailConsumer.java
@@ -3,12 +3,14 @@ package com.microservice.email.consumers;
 import com.microservice.email.dtos.EmailRecordDto;
 import com.microservice.email.models.EmailModel;
 import com.microservice.email.services.EmailService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class EmailConsumer {
 
@@ -17,6 +19,7 @@ public class EmailConsumer {
 
     @RabbitListener(queues = "${broker.queue.email.name}" )
     public void escutaFilaEmail(@Payload EmailRecordDto emailRecordDto) {
+        log.info("Email chegando");
         var emailModel = new EmailModel();
         BeanUtils.copyProperties(emailRecordDto, emailModel);
         emailService.enviarEmail(emailModel);
