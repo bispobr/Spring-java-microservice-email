a34a04765eef588fb5507a3cf9e91ee5ed3212ce
bispobr <156684382+bispobr@users.noreply.github.com>
2026-05-02 11:02:47 -0300

    feat: add profile test

--- src/test/java/com/microservice/email/EmailApplicationTests.java
+++ src/test/java/com/microservice/email/EmailApplicationTests.java
@@ -2,8 +2,10 @@ package com.microservice.email;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EmailApplicationTests {
 
 	@Test
